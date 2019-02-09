c302a2522e40b3749384714d046dee08a3010ec2
Jordy Sipkema <jordysipkema@gmail.com>
2019-02-09 12:09:06 +0100

    Setting up C++ compiler/linker settings

--- CVIS-2-TellenVanObjecten/CVIS-2-TellenVanObjecten/main.cpp
+++ CVIS-2-TellenVanObjecten/CVIS-2-TellenVanObjecten/main.cpp
@@ -8,8 +8,30 @@
 
 #include <iostream>
 
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/video/tracking.hpp>
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    cv::VideoCapture cap;
+    cap.open(0);
+    
+    if (!cap.isOpened()){
+        std::cerr << "***Could not initialize capturing...***" << std::endl;
+        std::cerr << "Current parameter's value: " << std::endl;
+        return -1;
+    }
+    
+    cv::Mat frame;
+    for(;;){
+        cap >> frame;
+        if (frame.empty()){
+            std::cerr << "Frame is empty" << std::endl;
+            break;
+        }
+        cv::imshow("Camera capture", frame);
+        cv::waitKey(10);
+    }
+    
+    return 1;
 }
